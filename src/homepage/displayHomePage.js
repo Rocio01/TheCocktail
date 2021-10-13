9f7146ea694a8941cd8be162aa75c2057aa2e851
Ajayi Peter <peterdgreat2016@gmail.com>
2021-10-13 18:41:06 +0200

    Ingredients displaying on Modal

--- src/homepage/displayHomePage.js
+++ src/homepage/displayHomePage.js
@@ -1,10 +1,12 @@
+import getDetails from './apidetails';
+
 const display = (obj) => {
   const cardsContainer = document.querySelector('.cards-container');
   obj.drinks.forEach((drink) => {
     const divCol = document.createElement('div');
     cardsContainer.appendChild(divCol);
     divCol.className = 'col';
-    divCol.innerHTML = `<div class='card h-100" id ="${drink.idDrink}'>
+    divCol.innerHTML = `<div class='card h-100' id ='${drink.idDrink}'>
                           <img src='${drink.strDrinkThumb}' class='card-img-top p-3' alt='...'>
                           <div class='card-body'>
                             <h5 class='card-title'>${drink.strDrink}</h5>
@@ -28,53 +30,71 @@ const display = (obj) => {
       modal.classList.remove('d-none');
       // get the id of the card
       const cardNodes = comment.parentNode.parentNode.parentNode.childNodes;
-      console.log(cardNodes);
+      // get id of the card
+      const parentNodesid = comment.parentElement.parentElement.parentElement.id;
+      const details = getDetails(parentNodesid);
+      // let detailData = [];
+      const detailData = details.then((data) => data.drinks[0]);
       const imgSrc = cardNodes[1].getAttribute('src');
       const title = cardNodes[3].childNodes[1].innerHTML;
       const div = document.createElement('div');
-      div.innerHTML = `<div class="card h-100">
-      <header  class= 'd-flex justify-content-end'>
-      <button class='btn-close'></button>
-    </header>
-      <img src='${imgSrc}' class='card-img-top p-3' alt='...'>
-      <div class='card-body'>
-        <h5 class='card-title text-center'>
-        ${title}
-        </h5>
-        <section class='comments d-flex flex-column align-items-center'>
-        <h4>
-          Comments(2)
-        </h4>
-        <span>03/11/2021 Alex: I would love to buy it</span>
-        <span>03/11/2021 Mia: I would love to buy it</span>
-        </section>
-        <section class='add-comment'>
-        <h3>Add a comment</h3>
-        <form>
-          <div class='mb-3'>
-            <input type='text' class='form-control'>
-          </div>
-          <div class='mb-3'>
-              <textarea class='form-control' placeholder='Leave a comment here' id='floatingTextarea'></textarea>
-          </div>
-          <button type='submit' class='btn btn-primary'>Submit</button>
-        </form>
-      </section>
-        </div>     
-      </div>
-  </div>`;
-      content.appendChild(div);
-      console.log(content);
-
-      const closeBtn = document.querySelectorAll('.btn-close');
-      console.log(closeBtn);
-      closeBtn.forEach((btn) => {
-        console.log(btn.childNodes);
-        btn.addEventListener('click', () => {
-          modal.classList.add('d-none');
-          content.removeChild(content.childNodes[1]);
+      detailData.then((data) => {
+        div.innerHTML = `<div class="card h-100">
+           <header  class= 'd-flex justify-content-end'>
+           <button class='btn-close'></button>
+         </header>
+           <img src='${imgSrc}' class='card-img-top p-3' alt='...'>
+           <div class='card-body'>
+             <h5 class='card-title text-center'>
+             ${title}
+             </h5>
+             <section class='ingredients'>
+              <h5 class = 'text-center'>Ingredients</h5>
+              <ul class = 'list-unstyled d-flex justify-content-around'>
+  <li>${data.strIngredient1}</li>
+  <li>${data.strIngredient2}</li>
+  <li>${data.strIngredient3}</li>
+  <li>${data.strIngredient4}</li>
+  <li>${data.strIngredient5}</li>
+  <li>${data.strIngredient6}</li>
+</ul>
+   </section>
+  
+             <section class='comments d-flex flex-column align-items-center'>
+             <h4>
+               Comments(2)
+             </h4>
+             <span>03/11/2021 Alex: I would love to buy it</span>
+             <span>03/11/2021 Mia: I would love to buy it</span>
+             </section>
+             <section class='add-comment'>
+             <h3>Add a comment</h3>
+             <form>
+               <div class='mb-3'>
+                 <input type='text' class='form-control'>
+               </div>
+               <div class='mb-3'>
+                   <textarea class='form-control' placeholder='Leave a comment here' id='floatingTextarea'></textarea>
+               </div>
+               <button type='submit' class='btn btn-primary'>Submit</button>
+             </form>
+           </section>
+             </div>     
+           </div>
+       </div>`;
+console.log(data);
+        const closeBtn = document.querySelectorAll('.btn-close');
+        console.log(closeBtn);
+        closeBtn.forEach((btn) => {
+          console.log(btn.childNodes);
+          btn.addEventListener('click', () => {
+            modal.classList.add('d-none');
+            content.removeChild(content.childNodes[1]);
+          });
         });
       });
+      content.appendChild(div);
+      console.log(content);
     });
   });
   hearts.forEach((heart) => {
